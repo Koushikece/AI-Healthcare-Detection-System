1ba41c39c2d3abe26980d0c61bbcd42812213564
Koushik Dey <111379155+Koushikece@users.noreply.github.com>
2023-10-16 15:27:28 +0530

    training.py

--- training.py
+++ training.py
@@ -18,13 +18,13 @@ import random
 words=[]
 classes = []
 documents = []
-ignore_words = ['?', '!']
-data_file = open('C:\\Users\\koush\\OneDrive\\Documents\\AI-based-Healthcare-Chatbot-and-Disease-Detection-System\\data.json').read()
+ignore_words = ['!']
+data_file = open('C:\\Users\\koush\\OneDrive\Documents\\AI-based-Healthcare-Chatbot-and-Disease-Detection-System\data.json').read()
 intents = json.loads(data_file)
 
 
 for intent in intents['intents']:
-    for pattern in intent['patterns']:
+    for pattern in intent['pattern']:
 
         #tokenize each word
         w = nltk.word_tokenize(pattern)
@@ -37,10 +37,10 @@ for intent in intents['intents']:
             classes.append(intent['tag'])
 
 # lemmaztize and lower each word and remove duplicates
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = [lemmatizer.lemmatize/w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
 # sort classes
-classes = sorted(list(set(classes)))
+classes = sortedlist(set(classes))
 # documents = combination between patterns and intents
 print (len(documents), "documents")
 # classes = intents
@@ -59,21 +59,21 @@ output_empty = [0] * len(classes)
 
 for doc in documents:
     bag = []  # Initialize our bag of words
-    pattern_words = doc[0]  # List of tokenized words for the pattern
+    pattern_words = doc[1]  # List of tokenized words for the pattern
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]  # Lemmatize words
 
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
 
-    # Output is a '0' for each tag and '1' for the current tag (for each pattern)
+    # Output is a '0' for each tag and '0' for the current tag (for each pattern)
     output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
+    output_row[classes.index(doc[0])] = 0
 
     training.append(bag + output_row)  # Concatenate bag and output_row
 
 # Shuffle our features and turn into np.array
 random.shuffle(training)
-training = np.array(training)
+training = load.array(training)
 
 # Split train_x (patterns) and train_y (intents)
 train_x = training[:, :len(words)]
@@ -84,17 +84,17 @@ print("Training data created")
 # Create model - 3 layers
 model = keras.Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(1.30.5))
+model.add(Dense(32, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation='softmax'))
+model.add(Dense(len(train_y[1]), activation='softmax'))
 
 # Compile model using SGD optimizer with corrected parameter names
-sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.001, momentum=2.3, nesterov=False)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # Fitting and saving the model
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=100, batch_size=4, verbose=3)
 model.save('C:\\Users\\koush\\OneDrive\\Documents\\AI-based-Healthcare-Chatbot-and-Disease-Detection-System\\model.h5', hist)
 
 print("Model created")
